a351a72141bbbaa68af30ab372f0058dce3d1ebb
yujiawei <yujiawei96@yeah.net>
2018-09-08 00:08:53 +0800

    update

--- Test/crawler/spiders/TestSpider.py
+++ Test/crawler/spiders/TestSpider.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from scrapy import FormRequest
 from scrapy.spiders import Spider
 from crawler.items import TestItem
 from crawler.settings import *
@@ -11,17 +12,33 @@ import time
 import copy
 import pymongo
 import json
-
+import pandas as pd
 class TestspiderSpider(Spider):
     name = 'CrawlerTest'
-    
 
     def start_requests(self):
-         start_urls = 'http://www.jiayuan.com/167625797'
-         item = TestItem()
-         yield Request(url = start_urls, callback = self.parse)
+        start_urls = pd.read_csv('C:\Users\T440-Yu\source\repos\Myspider\CrawlerJiayuan\crawler\spiders\url.csv')
+        start_urls = 'http://www.jiayuan.com/167625797'
+        login_url = 'http://login.jiayuan.com/'
+        item = TestItem()
+         
+        yield Request(url = login_url, callback = self.parse_login)
+
+    def parse_login(self, response):
+        login_data = {
+            'name':'18612209787',
+            'password':'shiyan5201314',
+            #'remem_pass':'on',
+            #'ajaxsubmit':'1',
+            #'ljg_login':'1',
+            }
+        yield FormRequest.from_response(response, formdata = login_data, callback = self.parse)
 
     def parse(self, response):
+        url = 'http://www.jiayuan.com/167625797'
+        yield Request(url = url, callback = self.parse_item)
+
+    def parse_item(self, response):
         title = response.xpath('//div[@class="fl f_gray_999"]/text()').extract()
         content = response.xpath('//ul//div[@class="fl pr"]/em/text()').extract()
         print(content)
